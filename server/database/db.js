7b087ba6444cf11025bace6ab872325202a2dfae
rohit-shamra-medha <rohit.sharma@medha.org.in>
2024-04-08 01:26:55 +0530

    client file added

--- server/database/db.js
+++ server/database/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const Connection = async () => {
   // const URL = `mongodb+srv://Rohit:Rohit_123@google-docs-clone.x0fo1mz.mongodb.net/?retryWrites=true&w=majority`;
   const url =
-    `mongodb+srv://${userName}:${password}@cluster0.cn1wheq.mongodb.net/?retryWrites=true&w=majority`;
+    "mongodb+srv://rohit19:Rohit_123@cluster0.cn1wheq.mongodb.net/?retryWrites=true&w=majority";
   try {
     await mongoose.set('strictQuery', false);
     await mongoose.connect(url, {
